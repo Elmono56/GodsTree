089caa635ad9e42ff4d73a5502f445adc8ae9e66
javiervasquez212 <60985490+javiervasquez212@users.noreply.github.com>
2021-10-24 23:07:31 -0600

    Cambios Main/Juegos.h

--- juego.h
+++ juego.h
@@ -10,51 +10,62 @@
 #include <windows.h>
 #include <time.h>
 #include <iostream>
+#include<conio.h>
+#include <string>
+ 
+using namespace std;
+
+
+#define KEY_X 120
+
+bool juego = true;
 
 void ejecutar(Pila pMazoCartas, priority_queue<Dios, vector<Dios>, CompareAge> *pMazoDioses){
 
-    while(!pMazoDioses->empty()){
+    while(juego){
+
         Carta pSiguiente = pMazoCartas.getCarta();
+        if(pSiguiente.getTipo() == "Muerte"){
+            Dios A = pMazoDioses->top();
+            pMazoDioses->pop();
 
-        if(pSiguiente.getTipo() == "Muerte")
-        {
+            cout << "----------------------------" << endl;
+            cout << A.getFieles() << " / " << A.getName() << endl;
+            pSiguiente.realizaraccion(&A,&A);
+            cout << A.getFieles() << " / " << A.getName() << endl;
+            cout << "----------------------------" << endl;
 
-        Dios A = pMazoDioses->top();
-        pMazoDioses->pop();
+        }else{
 
-        cout << "----------------------------" << endl;
+            Dios A = pMazoDioses->top();
+            pMazoDioses->pop();
+            Dios B = pMazoDioses->top();
+            pMazoDioses->pop();
 
-        cout << A.getFieles() << " / " << A.getName() << endl;
-
-        pSiguiente.realizaraccion(&A,&A);
-
-        cout << A.getFieles() << " / " << A.getName() << endl;
-
-        cout << "----------------------------" << endl;
+            cout << "----------------------------" << endl;
+            cout << A.getFieles() << " / " << A.getName() << "---------" << B.getFieles() << " / " << B.getName() << endl;
+            pSiguiente.realizaraccion(&A,&B);
+            cout << A.getFieles() << " / " << A.getName() << "---------" << B.getFieles() << " / " << B.getName() << endl;
+            cout << "----------------------------" << endl;
 
         }
-        else{
-
-        Dios A = pMazoDioses->top();
-        pMazoDioses->pop();
-        Dios B = pMazoDioses->top();
-        pMazoDioses->pop();
-
-        cout << "----------------------------" << endl;
-
-        cout << A.getFieles() << " / " << A.getName() << "---------" << B.getFieles() << " / " << B.getName() << endl;
-
-        pSiguiente.realizaraccion(&A,&B);
-
-        cout << A.getFieles() << " / " << A.getName() << "---------" << B.getFieles() << " / " << B.getName() << endl;
-
-        cout << "----------------------------" << endl;
-        }
-
         Sleep(3000);    
-        
     }
 }
+
+void finalizar(){
+
+    char key = getch();
+    int value = key;
+
+    if(value == KEY_X){
+
+        juego = false;
+    }
+}
+
+
+
 
 
 #endif
